fcd7d91549e7272fe7045e5a1029f6f7f5449990
davidr123 <davidradaalejandro@gmail.com>
2018-08-02 11:28:24 -0500

    coed

---
+++ app.js
@@ -0,0 +1,17 @@
+var express= require('express');
+var exphbs= require('express-handlebars');
+
+var http= require('http');
+
+http.createServer(
+                  
+                  function(req,res){
+    res.writeHead(200,{'Content-Type':'text-plain'});
+    res.end('Hello World');
+}
+).listen(3000);
+console.log("servidor en el pperto localhost:3000");
+                  
+                  
+               
+
---
+++ index.js
@@ -0,0 +1,29 @@
+var express= require('express');
+var exphbs= require('express-handlebars');
+
+var http= require('http');
+
+http.createServer(
+                  
+                  function(req,res){
+                      
+                      var path =req.url.toLowerCase();
+                      switch(path){
+                          case '':
+                              res.writeHead(200, {'Content-Type':'Text-plain'})
+                              res.end('Home');
+                              break;
+                          case '/about':
+                                res.writeHead(200,{'Content-Type':'Text-plain'})
+                         res.end('About');
+                      break;
+                      default:
+                                 
+    res.writeHead(404,{'Content-Type':'text-plain'});
+    res.end('Error Not Found');
+                      break;
+}
+    }
+).listen(3000);
+console.log("servidor en el puerto localhost:3000");
+console.log("enrutamiento");
